ff09a43f4b9707f90938d97d5954b6c8dec2de7c
cnohall <christopher.nohall@gmail.com>
2026-09-15 14:07:07 +0900

    add fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className="min-h-screen antialiased">
         <Script
-          src="https://testblockonomics.com/js/checkout.js"
+          src="https://stagingtest.blockonomics.co/js/checkout.js"
           strategy="beforeInteractive"
         />
         {children}
